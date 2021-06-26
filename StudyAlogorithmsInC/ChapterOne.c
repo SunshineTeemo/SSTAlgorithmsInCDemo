1ebc9ea93c1ed7c8ea8df0315adca6b97e764bdc
SST <meiyp@guoniufinance.com>
2021-06-26 15:22:55 +0800

    first commit

---
+++ StudyAlogorithmsInC/ChapterOne.c
@@ -0,0 +1,13 @@
+//
+//  ChapterOne.c
+//  StudyAlogorithmsInC
+//
+//  Created by 梅琰培 on 2021/6/26.
+//  Copyright © 2021 SunshineTeemo. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+
+
---
+++ StudyAlogorithmsInC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StudyAlogorithmsInC
+//
+//  Created by 梅琰培 on 2021/6/26.
+//  Copyright © 2021 SunshineTeemo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
